5e3ea00bef3523f3fc796a904b5e587dfbfa9795
miladz9889 <milad@hey.com>
2022-02-16 00:27:49 -0800

    removing unused packages

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart' as DotEnv;
 import 'package:my_fit_log_app/screens/dashboard/page/dashboard.dart';
 import 'firebase_options.dart';
 
