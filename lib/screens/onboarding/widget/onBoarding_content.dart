ae193bf286401fc50b185beb31ee91c2687d8efe
miladz9889 <milad@hey.com>
2022-02-21 00:51:24 -0800

    Fixed images and pubspec

--- lib/screens/onboarding/widget/onBoarding_content.dart
+++ lib/screens/onboarding/widget/onBoarding_content.dart
@@ -3,7 +3,6 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:percent_indicator/circular_percent_indicator.dart';
-
 import '../../../core/const/color_constants.dart';
 import '../../../core/const/data_constants.dart';
 import '../bloc/onboarding_bloc.dart';
